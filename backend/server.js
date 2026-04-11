7bbd48f409f566ca5b5009ac94bf639b824b090e
jaytaylor-dev <taylor.javaris@gmail.com>
2026-04-11 13:51:33 -0700

    Added new details to api calls for ec2 db

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,21 @@ const db = require('./db');
 const app = express();
 app.use(cors());
 
+app.get('/api/test-db', (req, res) => {
+    db.query('SELECT 1 + 1 AS result', (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(results[0]);
+    });
+});
+
+app.get('/api/ping', (req, res) => {
+        res.json({ message: "pong" });
+});
+
+app.get('/api/check-db', (req, res) => {
+        res.json({ test: "working" });
+});
+
 app.get('/api/hello', (req, res) => {
     db.query('SELECT text FROM messages LIMIT 1', (err, results) => {
         if(err) {
@@ -16,6 +31,6 @@ app.get('/api/hello', (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`We got a connection through -> ${PORT}`);
 });
